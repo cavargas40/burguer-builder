b9fc8d5ec1d7db0b8d8129a28ee49e35133cf974
Carlos Andres Vargas Lopez <cavargas40@gmail.com>
2020-02-21 18:31:56 -0500

    connecting state into build controls

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import '../BuildControl.css';
+import './BuildControl.css';
 
 const BuildControl = props => (
   <div className="BuildControl">
     <div className="Label">{props.label}</div>
     <button className="Less">Less</button>
-    <button className="More">More</button>
+    <button className="More" onClick={props.added}>More</button>
   </div>
 );
 
--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,10 +1,24 @@
 import React from 'react';
-import './BuildControls.css'
+import './BuildControls.css';
+import BuildControl from './BuildControl/BuildControl';
 
-const buildControls = (props) => (
-    <div>
+const controls = [
+  { label: 'Salad', type: 'salad' },
+  { label: 'Cheese', type: 'cheese' },
+  { label: 'Bacon', type: 'bacon' },
+  { label: 'Meat', type: 'meat' }
+];
 
-    </div>
+const BuildControls = props => (
+  <div className="BuildControls">
+    {controls.map(control => (
+      <BuildControl
+        key={control.label}
+        label={control.label}
+        added={() => props.ingredientAdded(control.type)}
+      />
+    ))}
+  </div>
 );
 
-export default buildControls;
+export default BuildControls;
