ceb0dbefeedf7aa0867829c7d7170560b381e0be
Carlos Andres Vargas Lopez <cavargas40@gmail.com>
2020-02-21 10:58:53 -0500

    starting implementation of burguer builder container

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './Layout.css';
 import Aux from '../../hoc/Auxiliar';
 
 const layout = props => (
   <Aux>
     <div>Toolbar, SideDrawer, Backdrop</div>
-    <main>{props.children}</main>
+    <main className="Content">{props.children}</main>
   </Aux>
 );
 
---
+++ src/containers/BurguerBuilder/BurguerBuilder.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Aux from '../../hoc/Auxiliar';
+
+class BurguerBuilder extends React.Component {
+  render() {
+    return (
+      <Aux>
+        <div>Burguer</div>
+        <div>Build Controls</div>
+      </Aux>
+    );
+  }
+}
+
+export default BurguerBuilder;
