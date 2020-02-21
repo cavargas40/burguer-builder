ec4c933a092b05fae8aa5a5b0879f19bca1f86d3
Carlos Andres Vargas Lopez <cavargas40@gmail.com>
2020-02-21 13:08:23 -0500

    outputting burger ingredients dynamically

--- src/components/Burguer/Burguer.js
+++ src/components/Burguer/Burguer.js
@@ -2,13 +2,19 @@ import React from 'react';
 import './Burguer.css';
 import BurguerIngredient from './BurguerIngredient/BurguerIngredient';
 
-const Burguer = () => {
+const Burguer = props => {
+  const tranformedIngredients = Object.keys(props.ingredients).map(
+    ingredient => {
+      return [...Array(props.ingredients[ingredient])].map((_, i) => {
+        return <BurguerIngredient key={ingredient + i} type={ingredient} />;
+      });
+    }
+  );
   return (
     <div className="Burguer">
-      <BurguerIngredient type="bread-top"/>
-      <BurguerIngredient type="cheese"/>
-      <BurguerIngredient type="meat"/>
-      <BurguerIngredient type="bread-bottom"/>
+      <BurguerIngredient type="bread-top" />
+      {tranformedIngredients}
+      <BurguerIngredient type="bread-bottom" />
     </div>
   );
 };
--- src/containers/BurguerBuilder/BurguerBuilder.js
+++ src/containers/BurguerBuilder/BurguerBuilder.js
@@ -3,11 +3,18 @@ import Aux from '../../hoc/Auxiliar';
 import Burguer from '../../components/Burguer/Burguer';
 
 class BurguerBuilder extends React.Component {
+  state = {
+    ingredients: {
+      salad: 1,
+      bacon: 1,
+      cheese: 2,
+      meat: 2
+    }
+  };
   render() {
-    
     return (
       <Aux>
-        <Burguer />
+        <Burguer ingredients={this.state.ingredients} />
         <div>Build Controls</div>
       </Aux>
     );
