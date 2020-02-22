bd26ec73ba0972d01f6d685333690af114b403df
Carlos Andres Vargas Lopez <cavargas40@gmail.com>
2020-02-21 22:10:50 -0500

    adding the price to the order summary

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -17,7 +17,10 @@ const OrderSummary = props => {
       <h3>Your Order</h3>
       <p>A delicious burger with the following ingredients:</p>
       <ul>{ingredientSummary}</ul>
-      <p>Continue to chekout?</p>
+      <p>
+        <strong>Total Price: ${props.price.toFixed(2)}</strong>
+      </p>
+      <p>Continue to checkout?</p>
       <Button btnType="Danger" clicked={props.purchaseCanceled}>
         CANCEL
       </Button>
