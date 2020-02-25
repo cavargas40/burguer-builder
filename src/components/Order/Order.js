3829da2ef248cc12ea66c791a6c3fc0d04272fd8
Carlos Andres Vargas Lopez <cavargas40@gmail.com>
2020-02-25 18:46:42 -0500

    outputting the orders

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,12 +1,37 @@
 import React from 'react';
 
-import './Order.css'
+import './Order.css';
 
-const Order = (props) => (
+const Order = props => {
+  const ingredients = Object.entries(props.ingredients).map(([key, value]) => {
+    return { name: key, amount: value };
+  });
+
+  const ingredientOutput = ingredients.map(({ name, amount }, index) => {
+    return (
+      <span
+        style={{
+          textTransform: 'capitalize',
+          display: 'inline-block',
+          margin: '0 8px',
+          border: '1px solid gray',
+          padding: '5px'
+        }}
+        key={index}
+      >
+        {name} ({amount})
+      </span>
+    );
+  });
+
+  return (
     <div className="Order">
-        <p>Ingredients: Salad (1)</p>
-        <p>Price: <strong> USD 5.45 </strong></p>
+      Ingredients: {ingredientOutput}
+      <p>
+        Price: <strong> USD {props.price.toFixed(2)} </strong>
+      </p>
     </div>
-);
+  );
+};
 
 export default Order;
