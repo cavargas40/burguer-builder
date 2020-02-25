557e888494b3fa3c5a730b16609dcdad150f44a2
Carlos Andres Vargas Lopez <cavargas40@gmail.com>
2020-02-25 15:46:38 -0500

    passing ingredients via query params

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -11,10 +11,10 @@ const CheckoutSummary = props => {
       <div style={{ width: '100%', margin: 'auto' }}>
         <Burger ingredients={props.ingredients} />
       </div>
-      <Button btnType="Danger" clicked={() => {}}>
+      <Button btnType="Danger" clicked={props.onCheckoutCancelled}>
         Cancel
       </Button>
-      <Button btnType="Danger" clicked={() => {}}>
+      <Button btnType="Danger" clicked={props.onCheckoutContinued}>
         Continue
       </Button>
     </div>
