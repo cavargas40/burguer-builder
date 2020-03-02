b14d92000dc2b5fb424f36f5423ffcd2805d9f5b
Carlos Andres Vargas Lopez <cavargas40@gmail.com>
2020-03-02 11:07:27 -0500

    getting a token from the backend

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { ActionTypes } from '../actions/actionTypes';
 
 export const authStart = () => {
@@ -23,5 +24,22 @@ export const authFail = error => {
 export const auth = (email, password) => {
   return dispatch => {
     dispatch(authStart);
+    axios
+      .post(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyACiXOSsGz-XwVYACzn5oO9fWVPdE5Hcns',
+        {
+          email,
+          password,
+          returnSecureToken: true
+        }
+      )
+      .then(response => {
+        console.log(response);
+        dispatch(authSuccess(response.data));
+      })
+      .catch(error => {
+        console.log(error);
+        dispatch(authFail(error));
+      });
   };
 };
