31e0f630f478e483b15120a83fc75d6e6a013aa9
Carlos Andres Vargas Lopez <cavargas40@gmail.com>
2020-03-01 22:32:05 -0500

    order reducer

---
+++ src/store/reducers/order.js
@@ -0,0 +1,31 @@
+import { ActionTypes } from '../actions/actionTypes';
+
+const initialState = {
+  orders: [],
+  loading: false
+};
+
+const orderReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ActionTypes.PurchaseBurgerSuccess:
+      const newOrder = {
+        ...action.orderData,
+        id: action.orderId
+      };
+      return {
+        ...state,
+        loading: false,
+        orders: [...state.orders, newOrder]
+      };
+    case ActionTypes.PurchaseBurgerFail:
+      return {
+          ...state,
+          loading: false,
+          
+      };
+    default:
+      return state;
+  }
+};
+
+export default orderReducer;
