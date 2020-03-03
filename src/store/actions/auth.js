c636db3ef7a9606e8ef8bfb544406f977afe7aea
Carlos Andres Vargas Lopez <cavargas40@gmail.com>
2020-03-03 01:32:53 -0500

    fixing connect + routing errors

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -79,18 +79,23 @@ export const setAuthRedirectPath = path => {
 };
 
 export const authCheckState = () => {
+  console.log('authCheck');
   return dispatch => {
     const token = localStorage.getItem('token');
     if (!token) {
       dispatch(logout());
     } else {
-      const expirationDate = new Date(localStorage.getItem('expiratonDate'));
-      if (expirationDate > new Date()) {
+      const expirationDate = new Date(localStorage.getItem('expirationDate'));
+      if (expirationDate <= new Date()) {
+        dispatch(logout());
+      } else {
         const userId = localStorage.getItem('userId');
         dispatch(authSuccess(token, userId));
-        dispatch(checkAuthTimeout(expirationDate.getSeconds() - new Date().getSeconds()))
-      } else {
-        dispatch(logout());
+        dispatch(
+          checkAuthTimeout(
+            (expirationDate.getTime() - new Date().getTime()) / 1000
+          )
+        );
       }
     }
   };
