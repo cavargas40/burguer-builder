977839c14ac8a353ca14c8084644797cb9329451
Carlos Andres Vargas Lopez <cavargas40@gmail.com>
2020-03-03 10:31:05 -0500

    minor fixes

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -14,7 +14,7 @@ const SideDrawer = props => {
   return (
     <Aux>
       <Backdrop show={props.open} clicked={props.closed} />
-      <div className={attachClasses.join(' ')}>
+      <div className={attachClasses.join(' ')} onClick={props.closed}>
         <div className="LogoContainer">
           <Logo />
         </div>
