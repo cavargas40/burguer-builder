4b7f502e3d4d0f29c1eb65babb6d2837dc4f4767
Carlos Andres Vargas Lopez <cavargas40@gmail.com>
2020-04-05 21:13:12 -0500

    Updates

---
+++ precache-manifest.483dd5acbb72f41b3236cd469dee9432.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6286fccc9441a542240e2a6c414f329",
+    "url": "/react-burger-builder/index.html"
+  },
+  {
+    "revision": "4365e85b4b303ecb015c",
+    "url": "/react-burger-builder/static/css/3.2c870daf.chunk.css"
+  },
+  {
+    "revision": "bae71fcee4557b20b580",
+    "url": "/react-burger-builder/static/css/4.f3e343b2.chunk.css"
+  },
+  {
+    "revision": "885f8b859525666b6e33",
+    "url": "/react-burger-builder/static/css/5.af8335d5.chunk.css"
+  },
+  {
+    "revision": "d1ede5ba880b5bab121f",
+    "url": "/react-burger-builder/static/css/main.d2e518a3.chunk.css"
+  },
+  {
+    "revision": "16b904c3cb4b2dba5a43",
+    "url": "/react-burger-builder/static/js/2.09e23939.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/react-burger-builder/static/js/2.09e23939.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4365e85b4b303ecb015c",
+    "url": "/react-burger-builder/static/js/3.3c5bc535.chunk.js"
+  },
+  {
+    "revision": "bae71fcee4557b20b580",
+    "url": "/react-burger-builder/static/js/4.12561c1a.chunk.js"
+  },
+  {
+    "revision": "885f8b859525666b6e33",
+    "url": "/react-burger-builder/static/js/5.f3e376b2.chunk.js"
+  },
+  {
+    "revision": "d1ede5ba880b5bab121f",
+    "url": "/react-burger-builder/static/js/main.e88d1018.chunk.js"
+  },
+  {
+    "revision": "ea0c7fe204bef9828d48",
+    "url": "/react-burger-builder/static/js/runtime-main.7eb8c1a8.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/react-burger-builder/static/media/burger-logo.b8503d26.png"
+  }
+]);
